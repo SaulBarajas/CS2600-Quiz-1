9cedcf7508ca15d7e4def86c262a2dd1447c60fe
SaulBarajas <98624770+SaulBarajas@users.noreply.github.com>
2022-03-03 06:47:00 +0000

    Description of the program

---
+++ README_Number_Guessing_game.c
@@ -0,0 +1,12 @@
+/*
+The program will allow a user to play a game in which they try to guess a number.
+The max number by default is 10.
+The user will have 3 options when starting the program:
+    1.Play the game 
+    2.Change the max number
+    3.quit
+Option 1: Starts the game and the user's trys are counted for every guess until they correctly guess the number.
+          When the game has started the user is able to hit q to return to menu.
+Option 2: Will allow a user to change the max number (cannot be negative or above default max).
+Option 3: Thank the user for playing, Print out result of games played, end the game.
+*/
