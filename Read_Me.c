540fad5fdc5a1b6f1cd8de0cd9d76ea5ce38eb41
Saul Barajas <saulbarajas@cpp.edu>
2022-03-02 19:08:10 -0800

    File explaining the program

---
+++ Read_Me.c
@@ -0,0 +1,10 @@
+/* 
+This program will compute the tax and tip of a restaurant bill for a patron.
+The program shouldd accept tax and tip as percentages from command line
+Displays the meal cost, tax amount, tip amount, and total.
+Cost should be randomly chosen between the following:
+Salad: $9.95
+Soup: $4.55
+Sandwich: $13.25
+Pizza: $22.35
+*/
